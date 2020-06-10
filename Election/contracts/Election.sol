bf79b0ccba86201810ec60ebc084282510849f13
parth jardosh <parthjardosh@gmail.com>
2020-06-10 19:45:35 +0530

    primary testing done

--- Election/contracts/Election.sol
+++ Election/contracts/Election.sol
@@ -1,11 +1,23 @@
 pragma solidity >=0.4.21 <0.7.0;
 contract Election{
-	//Store candidate
-	
-	//Read candidate
-	string public candidate;
-	//Constructor
+	// Model a Candidate
+	struct Candidate{
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	// Store Candidates
+	// fetch Candidates
+	mapping(uint => Candidate) public candidates;
+	// store Candidates count
+	uint public candidatesCount;
 	constructor() public {
-	candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string memory _name) private{
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
 	}
 }
