ae22ba4bec656834cf0c2e5e3fd07139c7272a02
parth jardosh <parthjardosh@gmail.com>
2020-06-10 19:03:23 +0530

    1.smoke test done

---
+++ Election/contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.4.21 <0.7.0;
+contract Election{
+	//Store candidate
+	
+	//Read candidate
+	string public candidate;
+	//Constructor
+	constructor() public {
+	candidate = "Candidate 1";
+	}
+}
